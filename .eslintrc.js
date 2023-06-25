e5ac4b5f85464e861756f115e297a98b9f3b01d1
howion <me@howion.com>
2023-06-25 18:59:31 +0000

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: {
+        browser: true,
+        commonjs: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        //-------------------------------------------------------------------------------------------------------------
+        // DISABLE
+        //-------------------------------------------------------------------------------------------------------------
+        '@typescript-eslint/no-empty-interface': [0],
+        '@typescript-eslint/no-explicit-any': [0],
+        '@typescript-eslint/no-var-requires': [0],
+        '@typescript-eslint/no-non-null-assertion': [0],
+        'react/prop-types': [0],
+        'no-control-regex': [0],
+
+        //-------------------------------------------------------------------------------------------------------------
+        // WARN
+        //-------------------------------------------------------------------------------------------------------------
+        '@typescript-eslint/ban-ts-comment': [
+            1,
+            {
+                'ts-expect-error': 'allow-with-description',
+                'ts-ignore': 'allow-with-description',
+                'ts-nocheck': 'allow-with-description',
+                'minimumDescriptionLength': 2
+            }
+        ],
+        'indent': [1, 4, { SwitchCase: 1 }],
+        'max-len': [1, { code: 120 }],
+
+        //-------------------------------------------------------------------------------------------------------------
+        // ERROR
+        //-------------------------------------------------------------------------------------------------------------
+        'linebreak-style': [2, 'windows'],
+        'quotes': [2, 'single'],
+        'semi': [2, 'never'],
+        '@typescript-eslint/explicit-module-boundary-types': [1, { allowArgumentsExplicitlyTypedAsAny: true }]
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# howion.js
+
+Hold on.
+
+## Installation
+
+...
+
+## Usage
+
+...
+
+## Todo's
+
+- [ ] multi package manager support?
+- [ ] deno
+- [ ] stylelint and postcss?
+
+## License
+
+MIT
